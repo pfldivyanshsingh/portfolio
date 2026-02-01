cc5def843b7d8c016032c84e102de8a0d58a124d
Divyansh Singh <pfldivyanshsingh@gmail.com>
2026-02-01 14:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+}, { threshold: 0.15 });
+
+sections.forEach(section => {
+  section.style.opacity = 0;
+  section.style.transform = "translateY(40px)";
+  section.style.transition = "0.6s ease";
+  observer.observe(section);
+});
+
+let resume = document.querySelector(".resumeee");
+resume.addEventListener('click', ()=>{
+  alert("Resume will be available Shortly.")
+})
